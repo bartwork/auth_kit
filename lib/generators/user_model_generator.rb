5251fe47b903550f91b2f01ca39d64d2c2ef6cb1
Alexander Bartenev <bartworkmail@gmail.com>
2022-06-14 00:35:10 +0300

    add generator

---
+++ lib/generators/user_model_generator.rb
@@ -0,0 +1,6 @@
+class UserModelGenerator < Rails::Generators::Base
+  desc "This generator creates model User at models/user.rb"
+  def create_initializer_file
+    create_file "models/user.rb", "# Add user model"
+  end
+end
